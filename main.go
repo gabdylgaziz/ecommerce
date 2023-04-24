d54b3a58882d63c26321eeba7656fb9d1e63bd9e
zhanel00 <zhanelgmstm@gmail.com>
2023-04-24 16:48:07 +0600

    Included sql script to reproduce database

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/gorilla/mux"
+	"net/http"
 )
+
 func main_page(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hello world")
 }
@@ -12,6 +13,6 @@ func main_page(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", main_page)
-    fmt.Println("server is started")
+	fmt.Println("server is started")
 	http.ListenAndServe(":2004", r)
 }
